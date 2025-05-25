3a5b1e942775d63d223750c2f8305f79354ad097
jonathan0145 <jonathan_bermeo014@hotmail.com>
2025-05-25 14:25:55 -0500

    corregido

--- apiGuerreros/src/routes/playerRoutes.js
+++ apiGuerreros/src/routes/playerRoutes.js
@@ -10,14 +10,14 @@ const router = express.Router();
 const { verifyRole } = require('../middleware/verifyRole.js');
 
 // Public routes
-router.post('/players', controller.register);
-router.post('/login', controller.login);
+router.post('/players', register);
+router.post('/login', login);
 
 // Routes for admin only
-router.post('/admin/players', verifyRole('admin'), controller.adminCreate);
-router.get('/admin/players', verifyRole('admin'), controller.getAll);
-router.get('/admin/players/:id', verifyRole('admin'), controller.getById);
-router.put('/admin/players/:id', verifyRole('admin'), controller.update);
-router.delete('/admin/players/:id', verifyRole('admin'), controller.remove);
+router.post('/admin/players', verifyRole('admin'), adminCreate);
+router.get('/admin/players', verifyRole('admin'), getAll);
+router.get('/admin/players/:id', verifyRole('admin'), getById);
+router.put('/admin/players/:id', verifyRole('admin'), update);
+router.delete('/admin/players/:id', verifyRole('admin'), remove);
 
 module.exports = router;

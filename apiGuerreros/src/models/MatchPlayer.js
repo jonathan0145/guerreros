01746531085f24c0dc48df4e8fa446e3249b02aa
jonathan0145 <jonathan_bermeo014@hotmail.com>
2025-06-20 16:36:07 -0500

    as

--- apiGuerreros/src/models/MatchPlayer.js
+++ apiGuerreros/src/models/MatchPlayer.js
@@ -1,30 +1,3 @@
-// const { DataTypes } = require('sequelize');
-// const sequelize = require('../../config/database');
-
-// const MatchPlayer = sequelize.define('MatchPlayer', {
-//     match_id: { 
-//         type: DataTypes.INTEGER, 
-//         primaryKey: true, 
-//          references: { model: 'matches', key: 'match_id' }
-//     },
-//     player_id: { 
-//         type: DataTypes.INTEGER,
-//         primaryKey: true,
-//         references: { model: 'players', key: 'player_id' }
-//     }
-// }, {
-//   tableName: 'match_players',
-//   timestamps: false
-// });
-
-// // *** ¡AÑADIR ESTE MÉTODO ASSOCIATE! ***
-// MatchPlayer.associate = (models) => {
-//     MatchPlayer.belongsTo(models.Match, { foreignKey: 'match_id' });
-//     MatchPlayer.belongsTo(models.Player, { foreignKey: 'player_id' });
-// };
-
-// module.exports = MatchPlayer;
-
 // src/models/MatchPlayer.js
 'use strict';
 const { Model } = require('sequelize');
